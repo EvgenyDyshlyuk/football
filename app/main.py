486e4f0be409e49e6af012388dde8fecbd7c07c1
Evgeny Dyshlyuk <48708976+EvgenyDyshlyuk@users.noreply.github.com>
2025-07-07 12:04:18 +0100

    Require auth for homepage via Cognito

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Request
-
-from app.auth.routes import auth_router
+from fastapi import FastAPI, Request, status
+from fastapi.responses import RedirectResponse
+from app.auth.routes import auth_router, COGNITO_AUTH_URL
 from app.jinja2_env import templates
 
 app = FastAPI()
@@ -10,4 +10,6 @@ app.include_router(auth_router, prefix="/auth")
 
 @app.get("/", include_in_schema=False)
 async def root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    if request.headers.get("Authorization") or request.query_params.get("code"):
+        return templates.TemplateResponse("home.html", {"request": request})
+    return RedirectResponse(COGNITO_AUTH_URL, status_code=status.HTTP_307_TEMPORARY_REDIRECT)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -17,26 +17,25 @@ def test_login_get():
     )
 
 
-def test_login_post_valid():
-    response = client.post(
-        "/auth/login",
-        data={"username": "admin", "password": "secret"},
-        follow_redirects=False,
+def test_login_post_redirect():
+    response = client.post("/auth/login", follow_redirects=False)
+    assert response.status_code == 307
+    assert (
+        response.headers.get("location")
+        == "https://eu-west-2wwv3xqgys.auth.eu-west-2.amazoncognito.com/login?client_id=68al97tfenubl3tc3k4fnii8ob&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A8000"
     )
-    assert response.status_code == 303
-    assert response.headers.get("location") == "/"
-
-
-def test_login_post_invalid():
-    response = client.post(
-        "/auth/login",
-        data={"username": "user", "password": "wrong"},
-    )
-    assert response.status_code == 200
-    assert "Invalid credentials" in response.text
 
 
 def test_homepage_get():
-    response = client.get("/")
+    response = client.get("/", headers={"Authorization": "Bearer token"})
     assert response.status_code == 200
     assert "Hello this is homepage" in response.text
+
+
+def test_homepage_redirect_for_new_user():
+    response = client.get("/", follow_redirects=False)
+    assert response.status_code == 307
+    assert (
+        response.headers.get("location")
+        == "https://eu-west-2wwv3xqgys.auth.eu-west-2.amazoncognito.com/login?client_id=68al97tfenubl3tc3k4fnii8ob&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A8000"
+    )
